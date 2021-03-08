f707db8970a6088183e2e9b6905f93dbf54e50dd
Jocalu <jocalu87@gmail.com>
2021-03-08 11:05:55 +0100

    Add userRestaurants test

--- MidDay-restaurant/backend/src/controller/userRestaurantController.js
+++ MidDay-restaurant/backend/src/controller/userRestaurantController.js
@@ -1,30 +1,36 @@
 const UserRestaurant = require('../models/userRestaurantModel');
+require('../models/restaurantModel');
 
 const userRestaurantController = () => {
-  const getUserRestaurant = async (req, res) => {
-    const { userId } = req.params;
-    await UserRestaurant
-      .findById(userId, (error, user) => {
-        if (error) {
-          res.status(500);
-          res.send('Error');
-        } else {
-          res.json(user);
-        }
-      });
-  };
-
-  const getAllUserRestaurants = async (req, res) => {
-    const allUsers = await UserRestaurant
-      .find({});
-    res.json(allUsers);
-  };
-
   const createUserRestaurant = (req, res) => {
     const newUser = new UserRestaurant(req.body);
     newUser.save(
       res.json(newUser),
     );
+  };
+
+  const getUserRestaurant = async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const user = await UserRestaurant
+        .findById(userId)
+        .populate('restaurant');
+      res.json(user);
+    } catch (error) {
+      res.status(500);
+      res.send('There was an error searching');
+    }
+  };
+
+  const getAllUserRestaurants = async (req, res) => {
+    try {
+      const allUsers = await UserRestaurant
+        .find({}).populate('restaurant');
+      res.json(allUsers);
+    } catch (error) {
+      res.status(500);
+      res.send('There was an error searching');
+    }
   };
 
   const updateUserRestaurant = async (req, res) => {
@@ -35,7 +41,8 @@ const userRestaurantController = () => {
 
     try {
       const updated = await UserRestaurant
-        .findByIdAndUpdate({ _id: userId }, { name, password }, { new: true });
+        .findByIdAndUpdate({ _id: userId }, { name, password }, { new: true })
+        .populate('restaurant');
       res.json(updated);
     } catch (error) {
       res.status(500);
@@ -43,16 +50,18 @@ const userRestaurantController = () => {
     }
   };
 
-  const deleteUserRestaurant = (req, res) => {
+  const deleteUserRestaurant = async (req, res) => {
     const { userId } = req.params;
-    UserRestaurant.findOneAndDelete({ _id: userId }, (error, deleted) => {
-      if (error) {
-        res.status(500);
-        res.send('Error');
-      } else {
-        res.json(deleted);
-      }
-    });
+
+    try {
+      const user = await UserRestaurant
+        .findByIdAndDelete({ _id: userId });
+
+      res.json(user);
+    } catch (error) {
+      res.status(500);
+      res.send('Error');
+    }
   };
 
   return {
