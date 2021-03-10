48873fe3f0919fd743a935c1f885ab997f93d4ae
Jocalu <jocalu87@gmail.com>
2021-03-10 09:57:44 +0100

    Fix dish router

--- MidDay-restaurant/backend/src/models/dishModel.js
+++ MidDay-restaurant/backend/src/models/dishModel.js
@@ -3,8 +3,8 @@ const { model, Schema } = require('mongoose');
 const dishSchema = new Schema({
   type: String,
   name: String,
-  ingredients:  { type: Schema.Types.ObjectId, ref: 'Ingredient' },
-  extra: Number
+  ingredients: { type: Schema.Types.ObjectId, ref: 'Ingredient' },
+  extra: Number,
 });
 
 module.exports = model('Dish', dishSchema);
