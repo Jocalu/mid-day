accfaf4c35557e09f12b3cad3a2ab89c3985bf6d
Jocalu <jocalu87@gmail.com>
2021-03-11 17:14:57 +0100

    Add model booking mock

--- MidDay-restaurant/frontend/src/app/pages/bookings/bookings.component.ts
+++ MidDay-restaurant/frontend/src/app/pages/bookings/bookings.component.ts
@@ -9,6 +9,73 @@ import { FormControl } from '@angular/forms'
 export class BookingsComponent implements OnInit {
   constructor () { }
 
+  hours:any[]=[
+    { hour: '12:00' },
+    { hour: '12:30' },
+    { hour: '13:00' },
+    { hour: '13:30' },
+    { hour: '14:00' },
+    { hour: '14:30' },
+    { hour: '15:00' },
+    { hour: '15:30' },
+    { hour: '16:00' }
+  ];
+
+  bookings:any[]=[
+    {
+      date: '02/04/2020',
+      hour: '12:30',
+      bookingName: 'Gerard',
+      pax: 3,
+      people: [{
+        name: 'Gerard',
+        allergies: 'altramuces, frutos secos',
+        selections: {
+          first: 'sopa',
+          second: 'carne',
+          dessert: 'yogurt'
+        }
+      },
+      {
+        name: 'Nil',
+        allergies: 'altramuces, frutos secos',
+        selections: {
+          first: 'ensalada',
+          second: 'pato',
+          dessert: 'crema'
+        }
+      },
+      {
+        name: 'Ignasi',
+        allergies: 'altramuces, frutos secos',
+        selections: {
+          first: 'ensalada',
+          second: 'pato',
+          dessert: 'crema'
+        }
+      }]
+    },
+    {
+      date: '02/04/2020',
+      hour: '12:30',
+      bookingName: 'Jose',
+      pax: 1,
+      people: [{
+        name: 'Jose',
+        allergies: 'altramuces, frutos secos',
+        selections: {
+          first: 'sopa',
+          second: 'carne',
+          dessert: 'yogurt'
+        }
+      }]
+    }
+  ]
+
+  reservedCapacity:number=55
+
+  maxCapacity:number = 100
+
   datepicker = new FormControl('')
 
   ngOnInit (): void {
