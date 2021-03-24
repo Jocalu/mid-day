07a99be752d99ab2fd0cd9d96081b7bb5a284be8
Jocalu <jocalu87@gmail.com>
2021-03-24 11:56:28 +0100

    Fix bookings styles

--- MidDay-restaurant/frontend/src/app/pages/dishes/dishes-search/dishes-search.component.ts
+++ MidDay-restaurant/frontend/src/app/pages/dishes/dishes-search/dishes-search.component.ts
@@ -20,9 +20,13 @@ export class DishesSearchComponent implements OnInit {
   ) {}
 
   openPopUp (id:string):void {
+    this.StoreSRV.deleteDish(id).subscribe()
+    this.dialog.closeAll()
+  }
+  /*   openPopUp (id:string):void {
     const dialog = this.dialog.open(PopupDishessearchComponent)
     dialog.componentInstance.dishId = id
-  }
+  } */
 
   dishSelected: HTMLElement
 
