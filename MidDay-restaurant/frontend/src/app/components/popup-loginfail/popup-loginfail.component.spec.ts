e6abd756eb6b56bd6d4a5f68a102e85a5c6ba5fe
Jocalu <jocalu87@gmail.com>
2021-03-24 17:22:08 +0100

    Fix tests frontend

--- MidDay-restaurant/frontend/src/app/components/popup-loginfail/popup-loginfail.component.spec.ts
+++ MidDay-restaurant/frontend/src/app/components/popup-loginfail/popup-loginfail.component.spec.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { MatDialogModule } from '@angular/material/dialog'
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { PopupLoginfailComponent } from './popup-loginfail.component'
@@ -13,7 +13,10 @@ describe('PopupLoginfailComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [PopupLoginfailComponent],
       imports: [MatDialogModule, BrowserAnimationsModule],
-
+      providers: [
+        MatDialog,
+        { provide: MatDialog, useValue: {} }
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
       .compileComponents()
@@ -27,5 +30,13 @@ describe('PopupLoginfailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should call closeAll', () => {
+    const spyFn = spyOn(component, 'closeDialog').and.callThrough()
+
+    component.closeDialog()
+
+    expect(spyFn).toHaveBeenCalled()
   })
 })
