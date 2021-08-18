50931c195d959d957cbd3b77bd6b1348b41abd5d
Jocalu <jocalu87@gmail.com>
2021-08-18 11:04:41 +0200

    Add environment example

---
+++ MidDay-restaurant/frontend/src/environments/environment.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+  }
+}
